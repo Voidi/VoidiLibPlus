b3df719967569fda119e04b002f119e31f5a5f16
Tobias Wohlfarth <tobias.wohlfarth@googlemail.com>
2026-09-10 00:30:31 +0200

    Fix: Access list of available dimensions

--- src/main/kotlin/name/voidi/mc/voidilibplus/Utils.kt
+++ src/main/kotlin/name/voidi/mc/voidilibplus/Utils.kt
@@ -5,13 +5,9 @@ import net.minecraft.resources.*
 import net.neoforged.neoforge.server.*
 
 
-val Dimensions: List<Identifier>
+val Dimensions: Set<Identifier>
 	get() {
 		// this seems to work on the physicial client
-		val regis = ServerLifecycleHooks.getCurrentServer()!!.registryAccess().lookup(Registries.DIMENSION).get()
-		return buildList {
-			for (entry in regis.entrySet()) {
-				add(entry.key.identifier())
-			}
-		}
+		val regis = ServerLifecycleHooks.getCurrentServer()?.registryAccess()?.lookupOrThrow(Registries.LEVEL_STEM)
+		return regis?.keySet().orEmpty()
 	}
